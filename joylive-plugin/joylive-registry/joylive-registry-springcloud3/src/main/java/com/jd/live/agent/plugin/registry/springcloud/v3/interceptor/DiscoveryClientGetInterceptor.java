202715b667f05fac6d6d344c56a3aca9f64ba286
hexiaofeng <hexiaofeng@jd.com>
2024-06-05 11:27:48 +0800

    Change exception type

--- joylive-plugin/joylive-registry/joylive-registry-springcloud3/src/main/java/com/jd/live/agent/plugin/registry/springcloud/v3/interceptor/DiscoveryClientGetInterceptor.java
+++ joylive-plugin/joylive-registry/joylive-registry-springcloud3/src/main/java/com/jd/live/agent/plugin/registry/springcloud/v3/interceptor/DiscoveryClientGetInterceptor.java
@@ -17,11 +17,15 @@ package com.jd.live.agent.plugin.registry.springcloud.v3.interceptor;
 
 import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
 import com.jd.live.agent.bootstrap.bytekit.context.MethodContext;
-import com.jd.live.agent.bootstrap.exception.RejectException;
 import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
 import com.jd.live.agent.governance.policy.PolicySupplier;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import reactor.core.publisher.Flux;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * DiscoveryClientGetInterceptor
@@ -39,7 +43,12 @@ public class DiscoveryClientGetInterceptor extends InterceptorAdaptor {
         MethodContext mc = (MethodContext) ctx;
         ServiceInstanceListSupplier supplier = (ServiceInstanceListSupplier) mc.getTarget();
         if (!policySupplier.isDone(supplier.getServiceId())) {
-            mc.setResult(Flux.error(new RejectException("Governance policy is not synchronized.")));
+            mc.setResult(Flux.error(HttpClientErrorException.create(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Governance policy is not synchronized.",
+                    new HttpHeaders(),
+                    new byte[0],
+                    StandardCharsets.UTF_8)));
             mc.setSkip(true);
         }
     }
