820e65dc172ac793fa836266e631b65880ad4943
hexiaofeng <hexiaofeng@jd.com>
2024-06-06 17:10:01 +0800

    Optimize registry type

--- joylive-plugin/joylive-registry/joylive-registry-dubbo2.7/src/main/java/com/jd/live/agent/plugin/registry/dubbo/v2_7/interceptor/InitServiceMetadataInterceptor.java
+++ joylive-plugin/joylive-registry/joylive-registry-dubbo2.7/src/main/java/com/jd/live/agent/plugin/registry/dubbo/v2_7/interceptor/InitServiceMetadataInterceptor.java
@@ -19,11 +19,9 @@ import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
 import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
 import com.jd.live.agent.governance.policy.PolicySupplier;
 import com.jd.live.agent.governance.policy.PolicyType;
-import org.apache.dubbo.config.AbstractInterfaceConfig;
-import org.apache.dubbo.config.AbstractReferenceConfig;
-import org.apache.dubbo.config.AbstractServiceConfig;
-import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.dubbo.common.constants.RegistryConstants.REGISTRY_TYPE_KEY;
@@ -60,6 +58,16 @@ public class InitServiceMetadataInterceptor extends InterceptorAdaptor {
             type = getRegistryType((AbstractReferenceConfig) config);
         }
         if ((type & REGISTRY_TYPE_SERVICE) > 0) {
+            if ((type & REGISTRY_TYPE_INTERFACE) == 0) {
+                // Only register with service mode
+                ApplicationConfig applicationConfig = config.getApplication();
+                Map<String, String> map = applicationConfig.getParameters();
+                if (map == null) {
+                    map = new HashMap<>();
+                    applicationConfig.setParameters(map);
+                }
+                map.put(REGISTRY_TYPE_KEY, SERVICE_REGISTRY_TYPE);
+            }
             policySupplier.subscribe(config.getApplication().getName(), PolicyType.SERVICE_POLICY);
         }
         if ((type & REGISTRY_TYPE_INTERFACE) > 0) {
