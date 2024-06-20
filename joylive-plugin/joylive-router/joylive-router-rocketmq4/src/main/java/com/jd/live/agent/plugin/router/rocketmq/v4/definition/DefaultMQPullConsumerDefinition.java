3d1188b35ec6a2467663e37138723e407fb94ad0
hexiaofeng <hexiaofeng@jd.com>
2024-06-20 10:15:11 +0800

    Add rocketmq client live and lane filter

---
+++ joylive-plugin/joylive-router/joylive-router-rocketmq4/src/main/java/com/jd/live/agent/plugin/router/rocketmq/v4/definition/DefaultMQPullConsumerDefinition.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.router.rocketmq.v4.definition;
+
+import com.jd.live.agent.core.bytekit.matcher.MatcherBuilder;
+import com.jd.live.agent.core.extension.annotation.ConditionalOnClass;
+import com.jd.live.agent.core.extension.annotation.ConditionalOnProperty;
+import com.jd.live.agent.core.extension.annotation.Extension;
+import com.jd.live.agent.core.inject.annotation.Config;
+import com.jd.live.agent.core.inject.annotation.Inject;
+import com.jd.live.agent.core.inject.annotation.Injectable;
+import com.jd.live.agent.core.instance.Application;
+import com.jd.live.agent.core.plugin.definition.InterceptorDefinition;
+import com.jd.live.agent.core.plugin.definition.InterceptorDefinitionAdapter;
+import com.jd.live.agent.core.plugin.definition.PluginDefinitionAdapter;
+import com.jd.live.agent.governance.config.GovernanceConfig;
+import com.jd.live.agent.plugin.router.rocketmq.v4.interceptor.SetConsumerGroupInterceptor;
+
+@Injectable
+@Extension(value = "DefaultMQPullConsumerDefinition_v5")
+@ConditionalOnProperty(name = {
+        GovernanceConfig.CONFIG_LIVE_ENABLED,
+        GovernanceConfig.CONFIG_LANE_ENABLED
+}, matchIfMissing = true)
+@ConditionalOnProperty(value = GovernanceConfig.CONFIG_LIVE_ROCKETMQ_ENABLED, matchIfMissing = true)
+@ConditionalOnClass(DefaultMQPullConsumerDefinition.TYPE_DEFAULT_MQ_PULL_CONSUMER)
+@ConditionalOnClass(PullAPIWrapperDefinition.TYPE_CLIENT_LOGGER)
+public class DefaultMQPullConsumerDefinition extends PluginDefinitionAdapter {
+
+    protected static final String TYPE_DEFAULT_MQ_PULL_CONSUMER = "org.apache.rocketmq.client.consumer.DefaultMQPullConsumer";
+
+    private static final String METHOD_SET_CONSUMER_GROUP = "setConsumerGroup";
+
+    private static final String[] ARGUMENT_SET_CONSUMER_GROUP = new String[]{
+            "java.lang.String"
+    };
+
+    @Inject(Application.COMPONENT_APPLICATION)
+    private Application application;
+
+    @Config(GovernanceConfig.CONFIG_LIVE_ENABLED)
+    private boolean liveEnabled = true;
+
+    @Config(GovernanceConfig.CONFIG_LANE_ENABLED)
+    private boolean laneEnabled = true;
+
+    public DefaultMQPullConsumerDefinition() {
+        this.matcher = () -> MatcherBuilder.named(TYPE_DEFAULT_MQ_PULL_CONSUMER);
+        this.interceptors = new InterceptorDefinition[]{
+                new InterceptorDefinitionAdapter(
+                        MatcherBuilder.named(METHOD_SET_CONSUMER_GROUP).
+                                and(MatcherBuilder.arguments(ARGUMENT_SET_CONSUMER_GROUP)),
+                        () -> new SetConsumerGroupInterceptor(application, liveEnabled, laneEnabled)
+                )
+        };
+    }
+}
