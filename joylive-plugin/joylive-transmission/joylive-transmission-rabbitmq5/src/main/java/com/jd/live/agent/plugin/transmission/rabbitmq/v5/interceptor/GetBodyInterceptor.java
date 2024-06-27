010dd0a2387d4df90eb2dc12ef3e70897a8afdd4
hexiaofeng <hexiaofeng@jd.com>
2024-06-27 11:53:21 +0800

    Add rabbitmq transmission

---
+++ joylive-plugin/joylive-transmission/joylive-transmission-rabbitmq5/src/main/java/com/jd/live/agent/plugin/transmission/rabbitmq/v5/interceptor/GetBodyInterceptor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.transmission.rabbitmq.v5.interceptor;
+
+import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
+import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
+import com.jd.live.agent.core.util.tag.Label;
+import com.jd.live.agent.governance.context.RequestContext;
+import com.jd.live.agent.governance.context.bag.CargoRequire;
+import com.jd.live.agent.governance.context.bag.CargoRequires;
+import com.jd.live.agent.governance.context.bag.Carrier;
+import com.rabbitmq.client.BasicProperties;
+import com.rabbitmq.client.Delivery;
+
+import java.util.List;
+
+public class GetBodyInterceptor extends InterceptorAdaptor {
+
+    private final CargoRequire require;
+
+    public GetBodyInterceptor(List<CargoRequire> requires) {
+        this.require = new CargoRequires(requires);
+    }
+
+    @Override
+    public void onEnter(ExecutableContext ctx) {
+        Boolean isProducer = RequestContext.getAttribute(Carrier.ATTRIBUTE_MQ_PRODUCER);
+        if (isProducer == null || !isProducer) {
+            restoreCargo((Delivery) ctx.getTarget());
+        }
+    }
+
+    private void restoreCargo(Delivery delivery) {
+        BasicProperties props = delivery.getProperties();
+        if (props != null) {
+            RequestContext.restore(delivery,
+                    () -> delivery.getEnvelope().getRoutingKey(),
+                    carrier -> carrier.addCargo(require, props.getHeaders(), Label::parseValue));
+        }
+    }
+
+}
---
+++ joylive-plugin/joylive-transmission/joylive-transmission-rabbitmq5/src/main/java/com/jd/live/agent/plugin/transmission/rabbitmq/v5/interceptor/PublishInterceptor.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.transmission.rabbitmq.v5.interceptor;
+
+import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
+import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
+import com.jd.live.agent.governance.context.RequestContext;
+import com.jd.live.agent.governance.context.bag.Carrier;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BasicProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PublishInterceptor extends InterceptorAdaptor {
+
+    @Override
+    public void onEnter(ExecutableContext ctx) {
+        RequestContext.setAttribute(Carrier.ATTRIBUTE_MQ_PRODUCER, Boolean.TRUE);
+        Object[] arguments = ctx.getArguments();
+        BasicProperties properties = (BasicProperties) arguments[arguments.length - 2];
+        if (properties == null) {
+            AMQP.BasicProperties p = new AMQP.BasicProperties().builder().headers(new HashMap<>()).build();
+            arguments[arguments.length - 2] = p;
+            properties = p;
+        }
+        Map<String, Object> headers = properties.getHeaders();
+        if (headers == null) {
+            headers = new HashMap<>();
+            AMQP.BasicProperties p = properties instanceof AMQP.BasicProperties ?
+                    ((AMQP.BasicProperties) properties).builder().headers(headers).build() :
+                    new AMQP.BasicProperties(
+                            properties.getContentType(), properties.getContentEncoding(), headers,
+                            properties.getDeliveryMode(), properties.getPriority(), properties.getCorrelationId(),
+                            properties.getReplyTo(), properties.getExpiration(), properties.getMessageId(),
+                            properties.getTimestamp(), properties.getType(), properties.getUserId(),
+                            properties.getAppId(), null);
+            arguments[arguments.length - 2] = p;
+        }
+        RequestContext.cargos(headers::put);
+    }
+
+}
