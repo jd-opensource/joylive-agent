07026682ceac25e668ea242db2fca4d5a2764c54
hexiaofeng <hexiaofeng@jd.com>
2024-07-10 15:50:45 +0800

    Fix npe

--- joylive-core/joylive-governance-api/src/main/java/com/jd/live/agent/governance/invoke/metadata/parser/ServiceMetadataParser.java
+++ joylive-core/joylive-governance-api/src/main/java/com/jd/live/agent/governance/invoke/metadata/parser/ServiceMetadataParser.java
@@ -66,7 +66,7 @@ public abstract class ServiceMetadataParser implements ServiceParser {
         String serviceName = parseServiceName();
         String serviceGroup = parseServiceGroup();
         Service service = parseService(serviceName);
-        String path = service.getServiceType().normalize(parsePath());
+        String path = service == null ? parsePath() : service.getServiceType().normalize(parsePath());
         String method = parseMethod();
         ServicePolicy servicePolicy = parseServicePolicy(service, serviceGroup, path, method);
         boolean isWrite = parseWrite(servicePolicy);
