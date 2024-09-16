ee8c80fb1f2fcaed8b5a10818214fa59be2a9bba
hexiaofeng <hexiaofeng@jd.com>
2024-09-16 12:36:33 +0800

    Reuse chain

--- joylive-plugin/joylive-router/joylive-router-springgateway3/src/main/java/com/jd/live/agent/plugin/router/springgateway/v3/interceptor/GatewayClusterInterceptor.java
+++ joylive-plugin/joylive-router/joylive-router-springgateway3/src/main/java/com/jd/live/agent/plugin/router/springgateway/v3/interceptor/GatewayClusterInterceptor.java
@@ -29,8 +29,8 @@ import com.jd.live.agent.plugin.router.springgateway.v3.cluster.GatewayCluster;
 import com.jd.live.agent.plugin.router.springgateway.v3.config.GatewayConfig;
 import com.jd.live.agent.plugin.router.springgateway.v3.request.GatewayClusterRequest;
 import com.jd.live.agent.plugin.router.springgateway.v3.response.GatewayClusterResponse;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
 import org.springframework.cloud.gateway.filter.*;
@@ -76,9 +76,7 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
 
     private final GatewayConfig config;
 
-    private final Map<Object, GatewayCluster> clusters = new ConcurrentHashMap<>();
-
-    private volatile FilterConfig filterConfig;
+    private final Map<Object, FilterConfig> clusters = new ConcurrentHashMap<>();
 
     public GatewayClusterInterceptor(InvocationContext context, GatewayConfig config) {
         this.context = context;
@@ -95,37 +93,17 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
     @Override
     public void onEnter(ExecutableContext ctx) {
         RequestContext.setAttribute(Carrier.ATTRIBUTE_GATEWAY, Boolean.TRUE);
-        MethodContext mc = (MethodContext) ctx;
-        ServerWebExchange exchange = ctx.getArgument(0);
-        Object target = ctx.getTarget();
-        Route route = exchange.getRequiredAttribute(GATEWAY_ROUTE_ATTR);
-        FilterConfig filterConfig = getFilterConfig(target);
-        List<GatewayFilter> filters = filterConfig.globalFilters;
-        GatewayFilter rewritePathFilter = null;
-        GatewayFilter delegateFilter;
-        for (GatewayFilter filter : route.getFilters()) {
-            delegateFilter = filter instanceof OrderedGatewayFilter ? ((OrderedGatewayFilter) filter).getDelegate() : null;
-            if (delegateFilter != null && delegateFilter.getClass().getName().equals(TYPE_REWRITE_PATH_FILTER)) {
-                rewritePathFilter = delegateFilter;
-            } else {
-                if (filters == filterConfig.globalFilters) {
-                    filters = new ArrayList<>(filterConfig.getGlobalFilters());
-                }
-                filters.add(filter);
-            }
-        }
-        AnnotationAwareOrderComparator.sort(filters);
-        GatewayFilterChain chain = new LiveGatewayFilterChain(filters);
-        boolean loadBalance = pareURI(exchange, route, rewritePathFilter);
-
         RetryConfig retryConfig = RequestContext.removeAttribute(GatewayConfig.ATTRIBUTE_RETRY_CONFIG);
-        GatewayCluster cluster = clusters.computeIfAbsent(target, v -> new GatewayCluster(filterConfig.getClientFactory()));
 
-        InvocationContext ic = loadBalance ? context : new HttpForwardContext(context);
-        ReactiveLoadBalancer.Factory<ServiceInstance> factory = loadBalance ? cluster.getClientFactory() : null;
-
+        ServerWebExchange exchange = ctx.getArgument(0);
+        FilterConfig filterConfig = clusters.computeIfAbsent(ctx.getTarget(), this::createFilterConfig);
+        LiveGatewayFilterChain chain = filterConfig.chain(exchange);
+        GatewayCluster cluster = filterConfig.getCluster();
+        InvocationContext ic = chain.isLoadBalance() ? context : new HttpForwardContext(context);
+        ReactiveLoadBalancer.Factory<ServiceInstance> factory = chain.isLoadBalance() ? cluster.getClientFactory() : null;
         GatewayClusterRequest request = new GatewayClusterRequest(exchange, chain, factory, retryConfig, config);
         OutboundInvocation<GatewayClusterRequest> invocation = new GatewayHttpOutboundInvocation<>(request, ic);
+
         CompletionStage<GatewayClusterResponse> response = cluster.invoke(invocation);
         CompletableFuture<Void> result = new CompletableFuture<>();
         response.whenComplete((v, t) -> {
@@ -137,46 +115,10 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
                 result.complete(null);
             }
         });
+
+        MethodContext mc = (MethodContext) ctx;
         mc.setResult(Mono.fromCompletionStage(result));
         mc.setSkip(true);
-    }
-
-    /**
-     * Parses the URI and returns a boolean indicating whether load balancing is used based on the given route information, the current ServerWebExchange object, and an optional GatewayFilter for rewriting the path.
-     *
-     * @param exchange          Represents the current ServerWebExchange object, which contains information about the request and response.
-     * @param route             Represents the current route information, including the path, host, port, etc.
-     * @param rewritePathFilter An optional GatewayFilter used to rewrite the path.
-     * @return A boolean indicating whether load balancing is used.
-     */
-    private boolean pareURI(ServerWebExchange exchange, Route route, GatewayFilter rewritePathFilter) {
-        URI routeUri = route.getUri();
-
-        String scheme = routeUri.getScheme();
-        String schemePrefix = null;
-        boolean hasAnotherScheme = routeUri.getHost() == null && routeUri.getRawPath() == null
-                && SCHEME_PATTERN.matcher(routeUri.getSchemeSpecificPart()).matches();
-        Map<String, Object> attributes = exchange.getAttributes();
-        if (hasAnotherScheme) {
-            schemePrefix = routeUri.getScheme();
-            attributes.put(GATEWAY_SCHEME_PREFIX_ATTR, schemePrefix);
-            routeUri = URI.create(routeUri.getSchemeSpecificPart());
-            scheme = routeUri.getScheme();
-        }
-
-        if (rewritePathFilter != null) {
-            rewritePathFilter.filter(exchange, new LiveGatewayFilterChain(Collections.emptyList()));
-        }
-        URI uri = exchange.getAttributeOrDefault(GATEWAY_REQUEST_URL_ATTR, exchange.getRequest().getURI());
-        boolean encoded = containsEncodedParts(uri);
-        uri = UriComponentsBuilder.fromUri(uri)
-                .scheme(routeUri.getScheme())
-                .host(routeUri.getHost())
-                .port(routeUri.getPort())
-                .build(encoded)
-                .toUri();
-        attributes.put(GATEWAY_REQUEST_URL_ATTR, uri);
-        return SCHEMA_LB.equals(scheme) || SCHEMA_LB.equals(schemePrefix);
     }
 
     /**
@@ -185,33 +127,28 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
      * @param target The target object.
      * @return The filter configuration.
      */
-    private FilterConfig getFilterConfig(Object target) {
-        if (filterConfig == null) {
-            FilterConfig config = new FilterConfig();
-            List<GatewayFilter> filters = getGatewayFilters(target);
-            for (GatewayFilter filter : filters) {
-                if (filter instanceof OrderedGatewayFilter) {
-                    filter = ((OrderedGatewayFilter) filter).getDelegate();
-                }
-                String filterClassName = filter.getClass().getName();
-                if (filterClassName.equals(TYPE_GATEWAY_FILTER_ADAPTER)) {
-                    GlobalFilter globalFilter = getGlobalFilter(filter);
-                    if (globalFilter instanceof ReactiveLoadBalancerClientFilter) {
-                        // skip ReactiveLoadBalancerClientFilter, because it's implement by RouteFilter
-                        config.setClientFactory(getLoadBalancerClientFactory(globalFilter));
-                    } else if (!globalFilter.getClass().getName().equals(TYPE_ROUTE_TO_REQUEST_URL_FILTER)) {
-                        // the filter is implement by parseURI
-                        config.addFilter(filter);
-                    }
-                }
+    private FilterConfig createFilterConfig(Object target) {
+        List<GatewayFilter> globalFilters = getGlobalFilters(target);
+        List<GatewayFilter> filters = new ArrayList<>(globalFilters.size());
+        LoadBalancerClientFactory factory = null;
+        for (GatewayFilter filter : globalFilters) {
+            if (filter instanceof OrderedGatewayFilter) {
+                filter = ((OrderedGatewayFilter) filter).getDelegate();
             }
-            synchronized (this) {
-                if (filterConfig == null) {
-                    filterConfig = config;
+            String filterClassName = filter.getClass().getName();
+            if (filterClassName.equals(TYPE_GATEWAY_FILTER_ADAPTER)) {
+                GlobalFilter globalFilter = getGlobalFilter(filter);
+                if (globalFilter instanceof ReactiveLoadBalancerClientFilter) {
+                    // skip ReactiveLoadBalancerClientFilter, because it's implement by RouteFilter
+                    factory = getLoadBalancerClientFactory(globalFilter);
+                } else if (!globalFilter.getClass().getName().equals(TYPE_ROUTE_TO_REQUEST_URL_FILTER)) {
+                    // the filter is implemented by parseURI
+                    filters.add(filter);
                 }
             }
         }
-        return filterConfig;
+
+        return new FilterConfig(target, filters, new GatewayCluster(factory));
     }
 
     /**
@@ -243,13 +180,13 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
     }
 
     /**
-     * Returns the list of gateway filters associated with the given target object.
+     * Returns the list of global gateway filters associated with the given target object.
      *
      * @param target The target object.
      * @return The list of gateway filters.
      */
     @SuppressWarnings("unchecked")
-    private List<GatewayFilter> getGatewayFilters(Object target) {
+    private List<GatewayFilter> getGlobalFilters(Object target) {
         return (List<GatewayFilter>) ClassUtils
                 .describe(target.getClass())
                 .getFieldList()
@@ -262,25 +199,31 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
      */
     private static class LiveGatewayFilterChain implements GatewayFilterChain {
 
+        protected static final GatewayFilterChain EMPTY_CHAIN = new LiveGatewayFilterChain(Collections.emptyList());
+
         private final List<GatewayFilter> filters;
+
+        @Getter
+        private final boolean loadBalance;
 
         private int index;
 
         LiveGatewayFilterChain(List<GatewayFilter> filters) {
-            this.filters = filters;
+            this(filters, false);
         }
 
-        LiveGatewayFilterChain(List<GatewayFilter> filters, int index) {
+        LiveGatewayFilterChain(List<GatewayFilter> filters, boolean loadBalance) {
             this.filters = filters;
-            this.index = index;
+            this.loadBalance = loadBalance;
         }
 
         @Override
         public Mono<Void> filter(ServerWebExchange exchange) {
+            final int idx = index++;
             return Mono.defer(() -> {
-                if (index < filters.size()) {
-                    GatewayFilter filter = filters.get(index);
-                    return filter.filter(exchange, new LiveGatewayFilterChain(filters, index + 1));
+                if (idx < filters.size()) {
+                    GatewayFilter filter = filters.get(idx);
+                    return filter.filter(exchange, this);
                 } else {
                     return Mono.empty(); // complete
                 }
@@ -293,26 +236,78 @@ public class GatewayClusterInterceptor extends InterceptorAdaptor {
      * A utility class that holds the configuration for gateway filters.
      */
     @Getter
-    @Setter
+    @AllArgsConstructor
     private static class FilterConfig {
 
-        /**
-         * A list of global filters to be applied to all routes.
-         */
-        private List<GatewayFilter> globalFilters = new ArrayList<>();
+        private final Object target;
+
+        private final List<GatewayFilter> globalFilters;
+
+        private final GatewayCluster cluster;
 
         /**
-         * The load balancer client factory used for load balancing.
-         */
-        private LoadBalancerClientFactory clientFactory;
-
-        /**
-         * Adds a filter to the list of global filters.
+         * Creates a new LiveGatewayFilterChain instance based on the given ServerWebExchange object and Route.
          *
-         * @param filter The filter to be added.
+         * @param exchange The ServerWebExchange object, which contains information about the request and response.
+         * @return A new LiveGatewayFilterChain instance.
          */
-        public void addFilter(GatewayFilter filter) {
-            globalFilters.add(filter);
+        public LiveGatewayFilterChain chain(ServerWebExchange exchange) {
+            Route route = exchange.getRequiredAttribute(GATEWAY_ROUTE_ATTR);
+            List<GatewayFilter> filters = globalFilters;
+            GatewayFilter rewritePathFilter = null;
+            GatewayFilter delegateFilter;
+            for (GatewayFilter filter : route.getFilters()) {
+                delegateFilter = filter instanceof OrderedGatewayFilter ? ((OrderedGatewayFilter) filter).getDelegate() : null;
+                if (delegateFilter != null && delegateFilter.getClass().getName().equals(TYPE_REWRITE_PATH_FILTER)) {
+                    rewritePathFilter = delegateFilter;
+                } else {
+                    if (filters == globalFilters) {
+                        filters = new ArrayList<>(getGlobalFilters());
+                    }
+                    filters.add(filter);
+                }
+            }
+            AnnotationAwareOrderComparator.sort(filters);
+            return new LiveGatewayFilterChain(filters, pareURI(exchange, route, rewritePathFilter));
         }
+
+        /**
+         * Parses the URI and returns a boolean indicating whether load balancing is used based on the given route information, the current ServerWebExchange object, and an optional GatewayFilter for rewriting the path.
+         *
+         * @param exchange          Represents the current ServerWebExchange object, which contains information about the request and response.
+         * @param route             Represents the current route information, including the path, host, port, etc.
+         * @param rewritePathFilter An optional GatewayFilter used to rewrite the path.
+         * @return A boolean indicating whether load balancing is used.
+         */
+        private boolean pareURI(ServerWebExchange exchange, Route route, GatewayFilter rewritePathFilter) {
+            URI routeUri = route.getUri();
+
+            String scheme = routeUri.getScheme();
+            String schemePrefix = null;
+            boolean hasAnotherScheme = routeUri.getHost() == null && routeUri.getRawPath() == null
+                    && SCHEME_PATTERN.matcher(routeUri.getSchemeSpecificPart()).matches();
+            Map<String, Object> attributes = exchange.getAttributes();
+            if (hasAnotherScheme) {
+                schemePrefix = routeUri.getScheme();
+                attributes.put(GATEWAY_SCHEME_PREFIX_ATTR, schemePrefix);
+                routeUri = URI.create(routeUri.getSchemeSpecificPart());
+                scheme = routeUri.getScheme();
+            }
+
+            if (rewritePathFilter != null) {
+                rewritePathFilter.filter(exchange, LiveGatewayFilterChain.EMPTY_CHAIN);
+            }
+            URI uri = exchange.getAttributeOrDefault(GATEWAY_REQUEST_URL_ATTR, exchange.getRequest().getURI());
+            boolean encoded = containsEncodedParts(uri);
+            uri = UriComponentsBuilder.fromUri(uri)
+                    .scheme(routeUri.getScheme())
+                    .host(routeUri.getHost())
+                    .port(routeUri.getPort())
+                    .build(encoded)
+                    .toUri();
+            attributes.put(GATEWAY_REQUEST_URL_ATTR, uri);
+            return SCHEMA_LB.equals(scheme) || SCHEMA_LB.equals(schemePrefix);
+        }
+
     }
 }
