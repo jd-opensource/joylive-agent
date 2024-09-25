35aa4d6cfc18c5374142c137df805aad9da602e1
shichaoyang <shichaoyang@jd.com>
2024-09-25 18:13:08 +0800

    Update: update comment

--- joylive-demo/joylive-demo-grpc/joylive-demo-grpc-consumer/src/main/java/com/jd/live/agent/demo/grpc/consumer/UserServiceController.java
+++ joylive-demo/joylive-demo-grpc/joylive-demo-grpc-consumer/src/main/java/com/jd/live/agent/demo/grpc/consumer/UserServiceController.java
@@ -16,23 +16,19 @@ public class UserServiceController {
 
     @GetMapping("/get")
     public String get(@RequestParam("id") Integer id) {
-        // 创建请求
         UserGetRequest request = UserGetRequest.newBuilder().setId(id).build();
-        // 执行 gRPC 请求
         UserGetResponse response = userServiceGrpc.get(request);
-        // 响应
         return response.getName();
     }
 
     @GetMapping("/create") // 为了方便测试，实际使用 @PostMapping
     public Integer create(@RequestParam("name") String name,
                           @RequestParam("gender") Integer gender) {
-        // 创建请求
         UserCreateRequest request = UserCreateRequest.newBuilder()
-                .setName(name).setGender(gender).build();
-        // 执行 gRPC 请求
+                .setName(name)
+                .setGender(gender)
+                .build();
         UserCreateResponse response = userServiceGrpc.create(request);
-        // 响应
         return response.getId();
     }
 
--- joylive-demo/joylive-demo-grpc/joylive-demo-grpc-provider/src/main/java/com/jd/live/agent/demo/grpc/provider/UserServiceGrpcImpl.java
+++ joylive-demo/joylive-demo-grpc/joylive-demo-grpc-provider/src/main/java/com/jd/live/agent/demo/grpc/provider/UserServiceGrpcImpl.java
@@ -1,5 +1,6 @@
 package com.jd.live.agent.demo.grpc.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.jd.live.agent.demo.grpc.service.api.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -13,24 +14,18 @@ public class UserServiceGrpcImpl extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
     public void get(UserGetRequest request, StreamObserver<UserGetResponse> responseObserver) {
-        logger.info("[get]");
-        // 创建响应对象
         UserGetResponse.Builder builder = UserGetResponse.newBuilder();
         builder.setId(request.getId())
-                .setName("没有昵称：" + request.getId())
+                .setName("hello world ：" + request.getId())
                 .setGender(request.getId() % 2 + 1);
-        // 返回响应
         responseObserver.onNext(builder.build());
         responseObserver.onCompleted();
     }
 
     @Override
     public void create(UserCreateRequest request, StreamObserver<UserCreateResponse> responseObserver) {
-        logger.info("[create]");
-        // 创建响应对象
         UserCreateResponse.Builder builder = UserCreateResponse.newBuilder();
         builder.setId((int) (System.currentTimeMillis() / 1000));
-        // 返回响应
         responseObserver.onNext(builder.build());
         responseObserver.onCompleted();
     }
