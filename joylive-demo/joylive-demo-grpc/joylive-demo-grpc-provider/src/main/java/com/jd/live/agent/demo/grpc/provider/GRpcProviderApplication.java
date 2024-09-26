805eefccc1050c8153f4bd2871e607ab27abc401
shichaoyang <shichaoyang@jd.com>
2024-09-26 17:24:29 +0800

    Update: consumer enhance config

--- joylive-demo/joylive-demo-grpc/joylive-demo-grpc-provider/src/main/java/com/jd/live/agent/demo/grpc/provider/GRpcProviderApplication.java
+++ joylive-demo/joylive-demo-grpc/joylive-demo-grpc-provider/src/main/java/com/jd/live/agent/demo/grpc/provider/GRpcProviderApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GRpcProviderApplication {
 
     public static void main(String[] args) {
-        System.out.println("----> start provider ");
         SpringApplication.run(GRpcProviderApplication.class, args);
     }
 
--- joylive-plugin/joylive-router/joylive-router-grpc/src/main/java/com/jd/live/agent/plugin/router/gprc/interceptor/GrpcServerInterceptor.java
+++ joylive-plugin/joylive-router/joylive-router-grpc/src/main/java/com/jd/live/agent/plugin/router/gprc/interceptor/GrpcServerInterceptor.java
@@ -1,63 +1,63 @@
-/*
- * Copyright © ${year} ${owner} (${email})
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.jd.live.agent.plugin.router.gprc.interceptor;
-
-import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
-import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
-import com.jd.live.agent.core.util.tag.Label;
-import com.jd.live.agent.governance.context.RequestContext;
-import com.jd.live.agent.governance.context.bag.CargoRequire;
-import com.jd.live.agent.governance.context.bag.CargoRequires;
-import io.grpc.*;
-
-import java.util.List;
-
-public class GrpcServerInterceptor extends InterceptorAdaptor {
-
-    private final CargoRequire require;
-
-    public GrpcServerInterceptor(List<CargoRequire> requires) {
-        this.require = new CargoRequires(requires);
-    }
-
-    @Override
-    public void onEnter(ExecutableContext ctx) {
-        Object target = ctx.getTarget();
-        if (target instanceof ServerBuilder) {
-            ((ServerBuilder<?>) target).intercept(new ServerInterceptor() {
-                @Override
-                public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
-                        ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-                    if (headers != null) {
-                        RequestContext.create().addCargo(require, headers.keys(),
-                                name -> Label.parseValue(headers.get(Metadata.Key.of(name, Metadata.ASCII_STRING_MARSHALLER))));
-                    }
-                    return next.startCall(new ForwardingServerCall<ReqT, RespT>() {
-                        @Override
-                        protected ServerCall<ReqT, RespT> delegate() {
-                            return call;
-                        }
-
-                        @Override
-                        public MethodDescriptor<ReqT, RespT> getMethodDescriptor() {
-                            return call.getMethodDescriptor();
-                        }
-                    }, headers);
-                }
-            });
-        }
-    }
-}
+///*
+// * Copyright © ${year} ${owner} (${email})
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//package com.jd.live.agent.plugin.router.gprc.interceptor;
+//
+//import com.jd.live.agent.bootstrap.bytekit.context.ExecutableContext;
+//import com.jd.live.agent.core.plugin.definition.InterceptorAdaptor;
+//import com.jd.live.agent.core.util.tag.Label;
+//import com.jd.live.agent.governance.context.RequestContext;
+//import com.jd.live.agent.governance.context.bag.CargoRequire;
+//import com.jd.live.agent.governance.context.bag.CargoRequires;
+//import io.grpc.*;
+//
+//import java.util.List;
+//
+//public class GrpcServerInterceptor extends InterceptorAdaptor {
+//
+//    private final CargoRequire require;
+//
+//    public GrpcServerInterceptor(List<CargoRequire> requires) {
+//        this.require = new CargoRequires(requires);
+//    }
+//
+//    @Override
+//    public void onEnter(ExecutableContext ctx) {
+//        Object target = ctx.getTarget();
+//        if (target instanceof ServerBuilder) {
+//            ((ServerBuilder<?>) target).intercept(new ServerInterceptor() {
+//                @Override
+//                public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+//                        ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+//                    if (headers != null) {
+//                        RequestContext.create().addCargo(require, headers.keys(),
+//                                name -> Label.parseValue(headers.get(Metadata.Key.of(name, Metadata.ASCII_STRING_MARSHALLER))));
+//                    }
+//                    return next.startCall(new ForwardingServerCall<ReqT, RespT>() {
+//                        @Override
+//                        protected ServerCall<ReqT, RespT> delegate() {
+//                            return call;
+//                        }
+//
+//                        @Override
+//                        public MethodDescriptor<ReqT, RespT> getMethodDescriptor() {
+//                            return call.getMethodDescriptor();
+//                        }
+//                    }, headers);
+//                }
+//            });
+//        }
+//    }
+//}
