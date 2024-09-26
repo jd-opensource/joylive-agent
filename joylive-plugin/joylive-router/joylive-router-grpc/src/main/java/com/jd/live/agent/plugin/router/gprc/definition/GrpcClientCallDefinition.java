6cbbae594d1f29271aa957f6f566e2f6b46ecd1b
shichaoyang <shichaoyang@jd.com>
2024-09-26 13:51:51 +0800

    Update: add grpc router module to pom.xml and add definition classes

---
+++ joylive-plugin/joylive-router/joylive-router-grpc/src/main/java/com/jd/live/agent/plugin/router/gprc/definition/GrpcClientCallDefinition.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.router.gprc.definition;
+
+import com.jd.live.agent.core.bytekit.matcher.MatcherBuilder;
+import com.jd.live.agent.core.extension.annotation.*;
+import com.jd.live.agent.core.plugin.definition.InterceptorDefinitionAdapter;
+import com.jd.live.agent.core.plugin.definition.PluginDefinition;
+import com.jd.live.agent.core.plugin.definition.PluginDefinitionAdapter;
+import com.jd.live.agent.governance.config.GovernanceConfig;
+import com.jd.live.agent.plugin.router.gprc.interceptor.ClientCallImplInterceptor;
+
+@Extension(value = "GrpcClientCallDefinition", order = PluginDefinition.ORDER_TRANSMISSION)
+@ConditionalOnProperties(value = {
+        @ConditionalOnProperty(value = GovernanceConfig.CONFIG_LIVE_ENABLED, matchIfMissing = true),
+        @ConditionalOnProperty(value = GovernanceConfig.CONFIG_LANE_ENABLED, matchIfMissing = true),
+        @ConditionalOnProperty(value = GovernanceConfig.CONFIG_FLOW_CONTROL_ENABLED, matchIfMissing = true)
+}, relation = ConditionalRelation.OR)
+@ConditionalOnClass(GrpcClientCallDefinition.TYPE_CLIENT_CALL_IMPL)
+public class GrpcClientCallDefinition extends PluginDefinitionAdapter {
+
+    public static final String TYPE_CLIENT_CALL_IMPL = "io.grpc.internal.ClientCallImpl";
+
+    private static final String METHOD_START = "start";
+
+    private static final String[] ARGUMENTS_START = new String[]{
+            "io.grpc.ClientCall.Listener",
+            "io.grpc.Metadata"
+    };
+
+
+    public GrpcClientCallDefinition() {
+        super(MatcherBuilder.named(TYPE_CLIENT_CALL_IMPL),
+                new InterceptorDefinitionAdapter(
+                        MatcherBuilder.named(METHOD_START).
+                                and(MatcherBuilder.arguments(ARGUMENTS_START)),
+                        new ClientCallImplInterceptor()));
+    }
+}
