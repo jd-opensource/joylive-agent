91466329efc796eae335d1c9aee560ee649df217
chenzhiguo <chenzhiguo@jd.com>
2024-10-21 18:45:56 +0800

    Update group filter

--- joylive-core/joylive-governance-api/src/main/java/com/jd/live/agent/governance/invoke/filter/route/GroupFilter.java
+++ joylive-core/joylive-governance-api/src/main/java/com/jd/live/agent/governance/invoke/filter/route/GroupFilter.java
@@ -18,15 +18,13 @@ package com.jd.live.agent.governance.invoke.filter.route;
 import com.jd.live.agent.core.Constants;
 import com.jd.live.agent.core.extension.annotation.Extension;
 import com.jd.live.agent.core.inject.annotation.Injectable;
-import com.jd.live.agent.governance.config.ServiceConfig;
 import com.jd.live.agent.governance.invoke.OutboundInvocation;
 import com.jd.live.agent.governance.invoke.RouteTarget;
 import com.jd.live.agent.governance.invoke.filter.RouteFilter;
 import com.jd.live.agent.governance.invoke.filter.RouteFilterChain;
 import com.jd.live.agent.governance.invoke.metadata.ServiceMetadata;
+import com.jd.live.agent.governance.policy.PolicyId;
 import com.jd.live.agent.governance.request.ServiceRequest.OutboundRequest;
-
-import java.util.Map;
 
 /**
  * Support service calls by configuring to filter instances of a specific group
@@ -40,14 +38,16 @@ public class GroupFilter implements RouteFilter {
     @Override
     public <T extends OutboundRequest> void filter(OutboundInvocation<T> invocation, RouteFilterChain chain) {
         ServiceMetadata serviceMetadata = invocation.getServiceMetadata();
-        ServiceConfig serviceConfig = serviceMetadata == null ? null : serviceMetadata.getServiceConfig();
-        Map<String, String> invokeMapping = serviceConfig == null ? null : serviceConfig.getInvokeGroups();
-        if (invokeMapping != null && !invokeMapping.isEmpty()) {
-            String targetGroup = invokeMapping.get(serviceMetadata.getServiceName());
-            if (targetGroup != null) {
-                RouteTarget target = invocation.getRouteTarget();
-                target.filter(endpoint -> endpoint.getLabel(Constants.LABEL_SERVICE_GROUP).equals(targetGroup));
-            }
+        String targetGroup = serviceMetadata.getServiceGroup();
+        if (targetGroup != null && !targetGroup.isEmpty()) {
+            RouteTarget target = invocation.getRouteTarget();
+            target.filter(endpoint -> endpoint.getLabel(Constants.LABEL_SERVICE_GROUP).equals(targetGroup));
+        } else {
+            RouteTarget target = invocation.getRouteTarget();
+            target.filter(endpoint -> endpoint.getLabel(Constants.LABEL_SERVICE_GROUP).equals(PolicyId.DEFAULT_GROUP)
+                    || endpoint.getLabel(Constants.LABEL_SERVICE_GROUP) == null
+                    || endpoint.getLabel(Constants.LABEL_SERVICE_GROUP).isEmpty()
+            );
         }
         chain.filter(invocation);
     }
