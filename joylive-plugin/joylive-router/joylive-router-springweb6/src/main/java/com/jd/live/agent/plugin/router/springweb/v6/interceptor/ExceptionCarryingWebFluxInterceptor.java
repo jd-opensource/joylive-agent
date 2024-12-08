518d811d795b7f5526c1d22f1bc36b3d93baca0f
hexiaofeng <hexiaofeng@jd.com>
2024-12-09 07:41:39 +0800

    update webflux

--- joylive-plugin/joylive-router/joylive-router-springweb6/src/main/java/com/jd/live/agent/plugin/router/springweb/v6/interceptor/ExceptionCarryingWebFluxInterceptor.java
+++ joylive-plugin/joylive-router/joylive-router-springweb6/src/main/java/com/jd/live/agent/plugin/router/springweb/v6/interceptor/ExceptionCarryingWebFluxInterceptor.java
@@ -16,40 +16,19 @@ import java.util.List;
 import java.util.Set;
 
 import static com.jd.live.agent.core.Constants.DEFAULT_HEADER_SIZE_LIMIT;
-import static com.jd.live.agent.core.util.ExceptionUtils.asString;
-import static com.jd.live.agent.core.util.ExceptionUtils.getExceptions;
+import static com.jd.live.agent.core.util.ExceptionUtils.*;
 
 /**
  * @author Axkea
  */
 public class ExceptionCarryingWebFluxInterceptor extends InterceptorAdaptor {
 
-    private static final Set<String> exclude = new HashSet<>(Arrays.asList(
-            "java.util.concurrent.ExecutionException",
-            "java.lang.reflect.InvocationTargetException"
-    ));
-
     @Override
     public void onEnter(ExecutableContext ctx) {
         Throwable t = ctx.getArgument(0);
-        String exceptionNames = asString(getExceptions(t, e -> !exclude.contains(e.getClass().getName())), ',', DEFAULT_HEADER_SIZE_LIMIT);
-        String message = t.getMessage();
         ServerWebExchange exchange = ctx.getArgument(3);
         HttpHeaders headers = exchange.getResponse().getHeaders();
 
-        if (exceptionNames != null && !exceptionNames.isEmpty()) {
-            List<String> header = new ArrayList<>(1);
-            header.add(exceptionNames);
-            headers.put(Constants.EXCEPTION_NAMES_LABEL, header);
-        }
-        if (message != null && !message.isEmpty()) {
-            try {
-                String encodeMessage = URLEncoder.encode(message, StandardCharsets.UTF_8.name());
-                List<String> header = new ArrayList<>(1);
-                header.add(encodeMessage);
-                headers.put(Constants.EXCEPTION_MESSAGE_LABEL, header);
-            } catch (UnsupportedEncodingException e) {
-            }
-        }
+        exceptionHeaders(t, headers::set);
     }
 }
