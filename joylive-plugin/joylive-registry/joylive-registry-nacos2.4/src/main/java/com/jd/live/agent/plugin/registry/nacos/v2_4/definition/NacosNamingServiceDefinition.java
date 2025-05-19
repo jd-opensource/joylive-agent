792802a4082d31115cc8f21f30861af575802a54
hexiaofeng <hexiaofeng@jd.com>
2025-05-19 16:42:38 +0800

    Fix nacos 2.3/2.4 api issue.

---
+++ joylive-plugin/joylive-registry/joylive-registry-nacos2.4/src/main/java/com/jd/live/agent/plugin/registry/nacos/v2_4/definition/NacosNamingServiceDefinition.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.registry.nacos.v2_4.definition;
+
+import com.jd.live.agent.core.bytekit.matcher.MatcherBuilder;
+import com.jd.live.agent.core.extension.annotation.ConditionalOnClass;
+import com.jd.live.agent.core.extension.annotation.Extension;
+import com.jd.live.agent.core.inject.annotation.Inject;
+import com.jd.live.agent.core.inject.annotation.Injectable;
+import com.jd.live.agent.core.plugin.definition.InterceptorDefinition;
+import com.jd.live.agent.core.plugin.definition.InterceptorDefinitionAdapter;
+import com.jd.live.agent.core.plugin.definition.PluginDefinition;
+import com.jd.live.agent.core.plugin.definition.PluginDefinitionAdapter;
+import com.jd.live.agent.governance.registry.CompositeRegistry;
+import com.jd.live.agent.governance.registry.Registry;
+import com.jd.live.agent.plugin.registry.nacos.v2_4.condition.ConditionalOnNacos24GovernanceEnabled;
+import com.jd.live.agent.plugin.registry.nacos.v2_4.interceptor.NacosNamingServiceConstructorInterceptor;
+import com.jd.live.agent.plugin.registry.nacos.v2_4.interceptor.NacosNamingServiceShutdownInterceptor;
+
+/**
+ * NacosNamingServiceDefinition
+ */
+@Injectable
+@Extension(value = "NacosNamingServiceDefinition", order = PluginDefinition.ORDER_REGISTRY)
+@ConditionalOnNacos24GovernanceEnabled
+@ConditionalOnClass(NacosNamingServiceDefinition.TYPE)
+public class NacosNamingServiceDefinition extends PluginDefinitionAdapter {
+
+    protected static final String TYPE = "com.alibaba.nacos.client.naming.NacosNamingService";
+
+    private static final String METHOD_SHUTDOWN = "shutdown";
+
+    @Inject(Registry.COMPONENT_REGISTRY)
+    private CompositeRegistry registry;
+
+    public NacosNamingServiceDefinition() {
+        this.matcher = () -> MatcherBuilder.named(TYPE);
+        this.interceptors = new InterceptorDefinition[]{
+                new InterceptorDefinitionAdapter(
+                        MatcherBuilder.isConstructor(), () -> new NacosNamingServiceConstructorInterceptor(registry)),
+                new InterceptorDefinitionAdapter(
+                        MatcherBuilder.named(METHOD_SHUTDOWN), () -> new NacosNamingServiceShutdownInterceptor(registry)),
+        };
+    }
+}
---
+++ joylive-plugin/joylive-registry/joylive-registry-nacos2.4/src/main/java/com/jd/live/agent/plugin/registry/nacos/v2_4/instance/NacosEndpoint.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.registry.nacos.v2_4.instance;
+
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.jd.live.agent.core.Constants;
+import com.jd.live.agent.core.util.option.Converts;
+import com.jd.live.agent.governance.instance.AbstractEndpoint;
+import com.jd.live.agent.governance.instance.EndpointState;
+import com.jd.live.agent.governance.registry.ServiceEndpoint;
+import com.jd.live.agent.governance.request.ServiceRequest;
+
+import java.util.Map;
+
+/**
+ * A class that represents an endpoint in the Nacos registry.
+ */
+public class NacosEndpoint extends AbstractEndpoint implements ServiceEndpoint {
+
+    public static final String KEY_SECURE = "secure";
+
+    public static final String KEY_GROUP = "group";
+
+    public static final String KEY_NAMESPACE = "namespace";
+
+    /**
+     * The instance associated with this endpoint.
+     */
+    private final Instance instance;
+
+    /**
+     * Creates a new NacosEndpoint object with the specified instance.
+     *
+     * @param instance the instance associated with this endpoint
+     */
+    public NacosEndpoint(Instance instance) {
+        this.instance = instance;
+    }
+
+    public NacosEndpoint(Instance instance, Boolean secure) {
+        this.instance = instance;
+        if (secure != null && secure) {
+            Map<String, String> metadata = instance.getMetadata();
+            if (metadata != null) {
+                metadata.putIfAbsent(Constants.LABEL_SECURE, "true");
+            }
+        }
+    }
+
+    @Override
+    public String getId() {
+        return instance.getInstanceId();
+    }
+
+    @Override
+    public String getService() {
+        return instance.getServiceName();
+    }
+
+    @Override
+    public String getHost() {
+        return instance.getIp();
+    }
+
+    @Override
+    public int getPort() {
+        return instance.getPort();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return Boolean.parseBoolean(getLabel(Constants.LABEL_SECURE));
+    }
+
+    @Override
+    public Map<String, String> getMetadata() {
+        return instance.getMetadata();
+    }
+
+    @Override
+    public String getLabel(String key) {
+        Map<String, String> metadata = instance.getMetadata();
+        return metadata == null ? null : metadata.get(key);
+    }
+
+    @Override
+    public EndpointState getState() {
+        if (!instance.isEnabled()) {
+            return EndpointState.DISABLE;
+        }
+        return instance.isHealthy() ? EndpointState.HEALTHY : EndpointState.WEAK;
+    }
+
+    @Override
+    public Integer getWeight(ServiceRequest request) {
+        return Converts.getInteger(getLabel(Constants.LABEL_WEIGHT), (int) (instance.getWeight() * DEFAULT_WEIGHT));
+    }
+}
