b41d411d0e5c9a6a3ab700b001075ba429571425
hexiaofeng <hexiaofeng@jd.com>
2025-07-07 17:42:27 +0800

    Add traffic event logger

---
+++ joylive-implement/joylive-event/joylive-event-traffic-logger/src/main/java/com/jd/live/agent/implement/event/logger/TrafficEventLogger.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.implement.event.logger;
+
+import com.jd.live.agent.bootstrap.logger.Logger;
+import com.jd.live.agent.bootstrap.logger.LoggerFactory;
+import com.jd.live.agent.core.event.EventHandler.EventProcessor;
+import com.jd.live.agent.core.event.Publisher;
+import com.jd.live.agent.core.event.Subscription;
+import com.jd.live.agent.core.extension.annotation.ConditionalOnProperty;
+import com.jd.live.agent.core.extension.annotation.Extension;
+import com.jd.live.agent.governance.config.GovernanceConfig;
+import com.jd.live.agent.governance.event.TrafficEvent;
+import com.jd.live.agent.governance.event.TrafficEvent.ActionType;
+import com.jd.live.agent.governance.event.TrafficEvent.RejectType;
+
+@Extension("TrafficEventLogger")
+@ConditionalOnProperty(value = GovernanceConfig.CONFIG_CIRCUIT_BREAK_LOG_ENABLED)
+public class TrafficEventLogger implements Subscription<TrafficEvent>, EventProcessor<TrafficEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrafficEventLogger.class);
+
+    @Override
+    public void process(TrafficEvent event) {
+        if (event.getActionType() == ActionType.REJECT && event.getRejectType() == RejectType.REJECT_CIRCUIT_BREAK) {
+            logger.error("Circuit break {} requests, service={}, path={}, method={} ", event.getRequests(), event.getService(), event.getPath(), event.getMethod());
+        }
+    }
+
+    @Override
+    public String getTopic() {
+        return Publisher.TRAFFIC;
+    }
+}
