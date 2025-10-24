0afea3c921f6175cdb5499242a792a1252571687
hexiaofeng <hexiaofeng@jd.com>
2025-10-24 12:49:33 +0800

    Update mcp parameter parser

--- joylive-plugin/joylive-application/joylive-application-springboot2/src/main/java/com/jd/live/agent/plugin/application/springboot/v2/util/SpringUtils.java
+++ joylive-plugin/joylive-application/joylive-application-springboot2/src/main/java/com/jd/live/agent/plugin/application/springboot/v2/util/SpringUtils.java
@@ -15,8 +15,8 @@
  */
 package com.jd.live.agent.plugin.application.springboot.v2.util;
 
-import com.jd.live.agent.bootstrap.util.type.FieldAccessor;
 import com.jd.live.agent.core.bootstrap.AppContext;
+import com.jd.live.agent.plugin.application.springboot.v2.util.param.CompositeSystemParameterFactory;
 import com.jd.live.agent.plugin.application.springboot.v2.util.port.PortDetector;
 import com.jd.live.agent.plugin.application.springboot.v2.util.port.PortDetectorFactory;
 import com.jd.live.agent.plugin.application.springboot.v2.util.port.PortInfo;
@@ -24,16 +24,12 @@ import com.jd.live.agent.plugin.application.springboot.v2.util.port.env.EnvPortD
 import com.jd.live.agent.plugin.application.springboot.v2.util.port.jmx.JmxPortDetectorFactory;
 import com.jd.live.agent.plugin.application.springboot.v2.util.port.web.WebPortDetectorFactory;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.context.request.RequestContextHolder;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Parameter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static com.jd.live.agent.bootstrap.util.type.FieldAccessorFactory.getAccessor;
 import static com.jd.live.agent.core.util.type.ClassUtils.loadClass;
 
 public class SpringUtils {
@@ -45,22 +41,6 @@ public class SpringUtils {
     private static final Class<?> CLASS_REST_CONTROLLER = loadClass(TYPE_REST_CONTROLLER, ResourceLoader.class.getClassLoader());
     private static final String TYPE_DISPATCHER_SERVLET = "org.springframework.web.servlet.DispatcherServlet";
     private static final Class<?> CLASS_DISPATCHER_SERVLET = loadClass(TYPE_DISPATCHER_SERVLET, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAKARTA_HTTP_SERVLET_REQUEST = "jakarta.servlet.http.HttpServletRequest";
-    private static final Class<?> CLASS_JAKARTA_HTTP_SERVLET_REQUEST = loadClass(TYPE_JAKARTA_HTTP_SERVLET_REQUEST, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAKARTA_HTTP_SERVLET_RESPONSE = "jakarta.servlet.http.HttpServletResponse";
-    private static final Class<?> CLASS_JAKARTA_HTTP_SERVLET_RESPONSE = loadClass(TYPE_JAKARTA_HTTP_SERVLET_RESPONSE, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAKARTA_HTTP_SESSION = "jakarta.servlet.http.HttpSession";
-    private static final Class<?> CLASS_JAKARTA_HTTP_SESSION = loadClass(TYPE_JAKARTA_HTTP_SESSION, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAVAX_HTTP_SERVLET_REQUEST = "javax.servlet.http.HttpServletRequest";
-    private static final Class<?> CLASS_JAVAX_HTTP_SERVLET_REQUEST = loadClass(TYPE_JAVAX_HTTP_SERVLET_REQUEST, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAVAX_HTTP_SERVLET_RESPONSE = "javax.servlet.http.HttpServletResponse";
-    private static final Class<?> CLASS_JAVAX_HTTP_SERVLET_RESPONSE = loadClass(TYPE_JAVAX_HTTP_SERVLET_RESPONSE, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_JAVAX_HTTP_SESSION = "jakarta.servlet.http.HttpSession";
-    private static final Class<?> CLASS_JAVAX_HTTP_SESSION = loadClass(TYPE_JAVAX_HTTP_SESSION, ResourceLoader.class.getClassLoader());
-    private static final String TYPE_SERVLET_REQUEST_ATTRIBUTES = "org.springframework.web.context.request.ServletRequestAttributes";
-    private static final Class<?> CLASS_SERVLET_REQUEST_ATTRIBUTES = loadClass(TYPE_SERVLET_REQUEST_ATTRIBUTES, ResourceLoader.class.getClassLoader());
-    private static final FieldAccessor ACCESSOR_REQUEST = getAccessor(CLASS_SERVLET_REQUEST_ATTRIBUTES, "request");
-    private static final FieldAccessor ACCESSOR_RESPONSE = getAccessor(CLASS_SERVLET_REQUEST_ATTRIBUTES, "response");
     private static final String TYPE_MONO = "reactor.core.publisher.Mono";
     private static final Class<?> CLASS_MONO = loadClass(TYPE_MONO, ResourceLoader.class.getClassLoader());
 
@@ -70,8 +50,6 @@ public class SpringUtils {
     private static final Class<?> API_RESOURCE_CONTROLLER_CLASS = loadClass(API_RESOURCE_CONTROLLER_TYPE, ResourceLoader.class.getClassLoader());
     private static final String SWAGGER2_CONTROLLER_WEB_MVC_TYPE = "springfox.documentation.swagger2.web.Swagger2ControllerWebMvc";
     private static final Class<?> SWAGGER2_CONTROLLER_WEB_MVC_CLASS = loadClass(SWAGGER2_CONTROLLER_WEB_MVC_TYPE, ResourceLoader.class.getClassLoader());
-
-    private static volatile Map<Class<?>, Supplier<Object>> SUPPLIERS;
 
     /**
      * Checks if current thread is a development reload thread
@@ -122,25 +100,7 @@ public class SpringUtils {
      * @return Supplier for system objects like HttpServletRequest/Response
      */
     public static Supplier<Object> getSystemSupplier(Parameter parameter) {
-        if (SUPPLIERS == null) {
-            synchronized (SpringUtils.class) {
-                if (SUPPLIERS == null) {
-                    Map<Class<?>, Supplier<Object>> map = new HashMap<>();
-                    if (isWebMvcEnabled()) {
-                        if (CLASS_JAKARTA_HTTP_SERVLET_REQUEST != null) {
-                            map.put(CLASS_JAVAX_HTTP_SERVLET_REQUEST, () -> ACCESSOR_REQUEST.get(RequestContextHolder.getRequestAttributes()));
-                            map.put(CLASS_JAVAX_HTTP_SERVLET_RESPONSE, () -> ACCESSOR_RESPONSE.get(RequestContextHolder.getRequestAttributes()));
-                        }
-                        if (CLASS_JAVAX_HTTP_SERVLET_REQUEST != null) {
-                            map.put(CLASS_JAVAX_HTTP_SERVLET_REQUEST, () -> ACCESSOR_REQUEST.get(RequestContextHolder.getRequestAttributes()));
-                            map.put(CLASS_JAVAX_HTTP_SERVLET_RESPONSE, () -> ACCESSOR_RESPONSE.get(RequestContextHolder.getRequestAttributes()));
-                        }
-                    }
-                    SUPPLIERS = map;
-                }
-            }
-        }
-        return SUPPLIERS.get(parameter.getType());
+        return CompositeSystemParameterFactory.INSTANCE.getSupplier(parameter);
     }
 
     /**
---
+++ joylive-plugin/joylive-application/joylive-application-springboot2/src/main/java/com/jd/live/agent/plugin/application/springboot/v2/util/param/SystemParameterFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © ${year} ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jd.live.agent.plugin.application.springboot.v2.util.param;
+
+import java.lang.reflect.Parameter;
+import java.util.function.Supplier;
+
+/**
+ * Factory interface for creating suppliers of system-level parameters.
+ */
+public interface SystemParameterFactory {
+
+    /**
+     * Returns a supplier for the given parameter.
+     *
+     * @param parameter the parameter to create a supplier for
+     * @return a supplier that provides the parameter value
+     */
+    Supplier<Object> getSupplier(Parameter parameter);
+
+}
